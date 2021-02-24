ec5ccdfed28fd7d43301a390d3d9546450c7edbd
Mazz <mag151@usask.ca>
2021-02-24 13:50:08 -0600

    Comments for AVL tree and AVL treeNode

--- A4/asn4-StarterCode/lib280-asn4/src/lib280/tree/AVLTree280.java
+++ A4/asn4-StarterCode/lib280-asn4/src/lib280/tree/AVLTree280.java
@@ -1,4 +1,30 @@
 package lib280.tree;
 
-public class AVLTree280 {
+import lib280.base.Dispenser280;
+import lib280.base.Searchable280;
+
+public class AVLTree280<I extends  Comparable<? super I>> extends OrderedSimpleTree280<I> implements Searchable280<I>, Dispenser280<I> {
+    /*
+    * Getters and setters for the tree
+    * */
+
+    /*
+     * Insert Method */
+
+    /*
+     * Delete method */
+
+    /*
+     * Root Node , returning the root node of the tree
+     * */
+
+    /*
+     * goRight, rotating to fix RR imbalance
+     * */
+
+    /*
+     * goLeft, rotating to fix LL imbalance
+     * */
+
+
 }
--- A4/asn4-StarterCode/lib280-asn4/src/lib280/tree/AVLTreeNode280.java
+++ A4/asn4-StarterCode/lib280-asn4/src/lib280/tree/AVLTreeNode280.java
@@ -1,4 +1,28 @@
 package lib280.tree;
 
-public class AVLTreeNode280 {
+public class AVLTreeNode280<I extends Comparable<? super I>> extends BinaryNode280 {
+    /**
+     * Construct a new node with item x.
+     *
+     * @param x the item placed in the new node
+     * @timing Time = O(1)
+     */
+    public AVLTreeNode280(Comparable x) {
+        super(x);
+    }
+
+
+    /*Height*/
+
+    /*getHeight*/
+
+    /*leftSide*/
+
+    /*rightSide*/
+
+    /*rightNode*/
+
+    /*leftNode*/
+
+    /*To String */
 }
